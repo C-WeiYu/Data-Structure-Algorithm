6251f8a514743f2f3bd54868d911d7931ca59dc5
莊崴宇 <55570378+C-WeiYu@users.noreply.github.com>
2019-11-07 19:49:11 +0800

    Create heap_sort_06170201.py

---
+++ HW2/heap_sort_06170201.py
@@ -0,0 +1,51 @@
+class Solution(object):
+    def heapify(self,nums,root,length):
+        left=2*root+1
+        right=2*root+2
+
+        if left < length and right <length:
+            if nums[left]>nums[right]:
+                if nums[left]>nums[root]:
+                    Max=left
+                else:
+                    Max=root
+            else:
+                if nums[right]>nums[root]:
+                    Max=right
+                else:
+                    Max=root
+        elif left<length and right>=length:
+            if nums[left]>nums[root]:
+                Max=left
+            else:
+                Max=root
+        else:
+            Max=root
+        if Max != root :
+            heapify_temp_root=nums[root]
+            nums[root] =nums[Max]
+            nums[Max]=heapify_temp_root
+            Solution.heapify(self,nums,Max,length)
+        
+    def heap_sort(self, nums):
+        """
+        :type nums: List[int] ex:[3,2,-4,6,4,2,19],[5,1,1,2,0,0]
+        :rtype: List[int] ex:[-4,2,2,3,4,6,19],[0,0,1,1,2,5]
+        """
+        ans=[]
+        n=len(nums)
+        if n%2 == 0:
+            for root in range((n//2)-1,-1,-1):
+                Solution.heapify(self,nums,root,n)
+        else:
+            for root in range(((n-1)//2)-1,-1,-1):
+                Solution.heapify(self,nums,root,n)
+            
+        for i in range(0,n):
+            ans.append(nums[0])
+            nums[0]=nums[n-1]
+            del nums[n-1]
+            n=n-1
+            Solution.heapify(self,nums,0,n)
+        ans=ans[::-1]
+        return ans

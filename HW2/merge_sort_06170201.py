fe8fe4cd503b899975799264b5116fad57ac23da
莊崴宇 <55570378+C-WeiYu@users.noreply.github.com>
2019-11-07 19:47:51 +0800

    Create merge_sort_06170201.py

---
+++ HW2/merge_sort_06170201.py
@@ -0,0 +1,42 @@
+###### merge_sort_ID.py
+
+class Solution(object):
+    def merge_sort(self, nums):
+        """
+        :type nums: List[int] ex:[3,2,-4,6,4,2,19],[5,1,1,2,0,0]
+        :rtype: List[int] ex:[-4,2,2,3,4,6,19],[0,0,1,1,2,5]
+        """
+        i=0
+        j=0
+        left=[]
+        right=[]
+        ans=[]
+        if len(nums)<=1:
+            return nums
+        else:
+            if len(nums)%2 == 0 :
+                left=nums[0:int(len(nums)/2)]
+                right=nums[int(len(nums)/2) : len(nums)]
+            else :
+                left=nums[0:(len(nums)+1)//2]
+                right=nums[(len(nums)+1)//2:len(nums)]
+        left=self.merge_sort(left)
+        right=self.merge_sort(right)
+        while len(ans)!= len(left+right):
+            if left[i]>right[j]:
+                ans.append(right[j])
+                j+=1
+            elif left[i]==right[j]:
+                ans.append(right[j])
+                j+=1
+            elif left[i]<right[j]:
+                ans.append(left[i])
+                i+=1
+            while i==len(left) and j != len(right):
+                ans.append(right[j])
+                j+=1
+            while j==len(right) and i != len(left):
+                ans.append(left[i])
+                i+=1
+        return ans        
+        

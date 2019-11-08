b9b729691e7448557c96b4a0c7d94f5777165ea6
莊崴宇 <55570378+C-WeiYu@users.noreply.github.com>
2019-11-08 10:54:36 +0800

    Create Binary Search Tree.py

---
+++ Binary Search Tree.py
@@ -0,0 +1,16 @@
+class Node(object):
+    def __init__(self,val):
+        self.parent = None
+        self.left = None
+        self.right = None
+        self.element=val
+       
+class Binary Tree(object):
+    def __init__(self):
+        self.root=None
+        self.leaf=None
+        self.size=None
+    
+    def insert(self,val):
+        if root :
+          

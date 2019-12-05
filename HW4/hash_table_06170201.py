c8accd9df91cfd4485c74f1e8d8fdf24c23899f2
莊崴宇 <55570378+C-WeiYu@users.noreply.github.com>
2019-12-06 00:12:22 +0800

    Add files via upload

---
+++ HW4/hash_table_06170201.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[28]:
+
+
+from Cryptodome.Hash import MD5
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+        """
+        :type val: int
+        :type next: ListNode
+        :rtype: None        
+        """
+class MyHashSet:
+    def __init__(self, capacity=5):
+        self.capacity = capacity
+        self.data = [None] * capacity
+        """
+        :type capacity: int
+        :rtype: None
+        """
+    def add(self, key):
+        """
+        :type key: str
+        :rtype: None
+        """
+        key_int = MyHashSet.Hash(self, key)
+        index = key_int % self.capacity
+        node_head=self.data[index]
+        if node_head:
+            while node_head.next:
+                if node_head.val == key_int:  
+                    return
+                else:
+                    node_head=node_head.next  
+            if node_head.val != key_int:
+                node_head.next=ListNode(key_int) 
+            else:
+                return
+        else:
+            node_head=ListNode(key_int)
+            self.data[index]=node_head
+    def remove(self, key):
+        """
+        :type key: str
+        :rtype: None
+        """
+        key_int = MyHashSet.Hash(self, key)
+        index = key_int % self.capacity
+        node_head = self.data[index]
+        if node_head:
+            if node_head.val == key_int :
+                node_head=node_head.next
+                self.data[index] = node_head
+
+        if node_head : 
+            while node_head.next:
+                if node_head.next.val == key_int:
+                    node_head.next = node_head.next.next
+                else:
+                    node_head = node_head.next
+            
+
+    def contains(self, key):
+        """
+        :type key: str
+        :rtype: bool(True or False)
+        """
+        key_int = MyHashSet.Hash(self, key)
+        index = key_int % self.capacity
+        node_head=self.data[index]
+        while node_head != None:
+            if node_head.val == key_int:
+                return True
+            else:
+                node_head = node_head.next
+        else:
+            return False
+
+    def Hash(self, key):
+        h = MD5.new()
+        h.update(key.encode('utf-8'))
+        new_key=int(h.hexdigest(),16)
+        return new_key
+

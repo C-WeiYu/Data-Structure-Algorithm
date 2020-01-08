06a225d20ff25e56fafead0c46ca8c1342358adc
莊崴宇 <55570378+C-WeiYu@users.noreply.github.com>
2020-01-08 23:06:11 +0800

    Create #9_Mex Function.py

---
+++ CodeSignal/Python/#9_Mex Function.py
@@ -0,0 +1,9 @@
+def mexFunction(s, upperBound):
+    found = -1
+    for i in range(upperBound):
+        if not i in s:
+            found = i
+            break
+    else:
+        return upperBound
+    return found
